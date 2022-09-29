60955c14ef87d4f14b9b5063958d182511f1b897
davebrown1975 <dave@tucanoo.com>
2022-09-29 12:02:15 +0200

    Initial commit

---
+++ src/main/groovy/com/example/dependencytest/DependencytestApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.dependencytest
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DependencytestApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(DependencytestApplication, args)
+    }
+
+}
---
+++ src/test/groovy/com/example/dependencytest/DependencytestApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.example.dependencytest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DependencytestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
